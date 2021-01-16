8d7e5bc37472e691acaa3bb12c3069f9b2525649
vovamisjul <vovamisjul@gmail.com>
2021-01-16 16:58:46 +0300

    Used another api for jokes/facts/quotes

--- story_teller.py
+++ story_teller.py
@@ -1,26 +1,24 @@
+import json
 from typing import Optional
-import xml.etree.ElementTree as ET
+
 import aiohttp
 
-titles = {
-    "анекдот": 1,
-    "рассказ": 2,
-    "стишок": 3,
-    "тост": 6,
-    "анекдот 18+": 11,
-    "стишок 18+": 13,
-    "тост 18+": 16,
+urls = {
+    "анекдот": "generator-anekdotov-online",
+    "цитату": "generator-citat-online",
+    "факт": "generator-interesnykh-faktov",
 }
+
+base_url = "https://castlots.org/"
 
 
 async def get(title: str) -> Optional[str]:
-    if title in titles:
+    if title in urls:
         async with aiohttp.ClientSession() as session:
-            async with session.get("http://rzhunemogu.ru/Rand.aspx",
-                                   params={"CType": titles[title]}) as response:
-                content = await response.text(encoding="windows-1251")
-                root = ET.fromstring(content)
-                for child in root:
-                    if child.tag == "content":
-                        return child.text
+            async with session.post(f"https://castlots.org/{urls[title]}/generate.php",
+                                    headers={"X-Requested-With": "XMLHttpRequest"},
+                                    data={"hid": "yes"}) as response:
+                content = await response.text()
+                content = json.loads(content)
+                return content["va"]
     return None
